cbd33691fd70ea9027c30ad93a39e07b4d589275
Myllena Lucena <karensantana278@gmail.com>
2021-12-07 23:59:19 -0300

    componente header

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './styles.css';
 
 export default ({item}) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
     return(
         <section className ="featured" style={{ 
             backgroundSize: 'cover',
@@ -10,10 +17,36 @@ export default ({item}) => {
 
 
         }}>
-            
+           
             <div className="featured--vertical">
+                
                 <div className="featured--horizontal">
+                    <div className="featured--name">
+                        {item.name}
                     </div>
+                    <div className="featured--info">
+                        <div className="featured--points">
+                            {item.vote_average} pontos
+                        </div>
+                        <div className="featured--year">
+                            {firstDate.getFullYear()}
+                        </div>
+                        <div className="featured--seasons">
+                            {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's': ''}
+                        </div>
+                    </div>
+                    <div className="featured--description">
+                        {item.overview}
+                    </div>
+                    <div className="featured--buttons">
+                        <a href={`/watch/${item.id}`} className="featured--assistir">► Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--lista">+ Minha Lista</a>
+                    </div>
+                    <div className="featured--genres">
+                        <strong>Gêneros: </strong>
+                        {genres.join(', ')}
+                    </div>
+                </div>
             </div>
            
         </section>
